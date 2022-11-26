d168e2ecd34e61d402a7a494a56fda147862a694
Kadir Yılmaz <acatay@outlook.com>
2022-11-26 10:42:23 +0300

    Make codes optimized in navbar links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,57 @@ import { FormattedMessage } from "react-intl";
 export default function Header() {
   const router = useRouter()
 
+  const navLinks = [
+    {
+      href: "/",
+      name: "home",
+      icon: <HiOutlineHome size={28} />,
+      activeIcon: <HiHome size={28} />
+    },
+    {
+      href: "/explore",
+      name: "explore",
+      icon: <HiOutlineHashtag size={28} />,
+      activeIcon: <HiHashtag size={28} />
+    },
+    {
+      href: "/notifications",
+      name: "notifications",
+      icon: <HiOutlineBell size={28} />,
+      activeIcon: <HiBell size={28} />
+    },
+    {
+      href: "/messages",
+      name: "messages",
+      icon: <HiOutlineAnnotation size={28} />,
+      activeIcon: <HiAnnotation size={28} />
+    },
+    {
+      href: "/bookmarks",
+      name: "bookmarks",
+      icon: <HiOutlineBookmark size={28} />,
+      activeIcon: <HiBookmark size={28} />
+    },
+    {
+      href: "/lists",
+      name: "lists",
+      icon: <HiOutlineDocumentText size={28} />,
+      activeIcon: <HiDocumentText size={28} />
+    },
+    {
+      href: "/profile",
+      name: "profile",
+      icon: <HiOutlineUser size={28} />,
+      activeIcon: <HiUser size={28} />
+    },
+    {
+      href: "/settings/accessibility_display_and_languages",
+      name: "more",
+      icon: <HiOutlineDotsCircleHorizontal size={28} />,
+      activeIcon: <HiDotsCircleHorizontal size={28} />
+    },
+  ]
+
   return (
     <header>
       <div className="header-main">
@@ -15,86 +66,19 @@ export default function Header() {
           <AiOutlineTwitter size={36} />
         </Link>
         <nav className='nav-links'>
-          <Link href="/" className={router.pathname === "/" ? 'active' : ''}>
-            {router.pathname === "/" ? (
-              <HiHome size={28} />
+          
+          {navLinks.map((nav, index) => (
+          <Link key={index} href={nav.href} className={router.pathname === nav.href ? 'active' : ''}>
+            {router.pathname === nav.href ? (
+              nav.activeIcon
             ) : (
-              <HiOutlineHome size={28} />
+              nav.icon
             )}
             <span>
-              <FormattedMessage id="page.home" />
+              <FormattedMessage id={`page.${nav.name}`} />
             </span>
           </Link>
-          <Link href="/explore" className={router.pathname === "/explore" ? 'active' : ''}>
-            {router.pathname === "/explore" ? (
-              <HiHashtag size={28} />
-            ) : (
-              <HiOutlineHashtag size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.explore" />
-            </span>
-          </Link>
-          <Link href="/notifications" className={router.pathname.includes("/notifications") ? 'active' : ''}>
-            {router.pathname.includes("/notifications") ? (
-              <HiBell size={28} />
-            ) : (
-              <HiOutlineBell size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.notifications" />
-            </span>
-          </Link>
-          <Link href="/messages" className={router.pathname === "/messages" ? 'active' : ''}>
-            {router.pathname === "/messages" ? (
-              <HiAnnotation size={28} />
-            ) : (
-              <HiOutlineAnnotation size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.messages" />
-            </span>
-          </Link>
-          <Link href="/bookmarks" className={router.pathname === "/bookmarks" ? 'active' : ''}>
-            {router.pathname === "/bookmarks" ? (
-              <HiBookmark size={28} />
-            ) : (
-              <HiOutlineBookmark size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.bookmarks" />
-            </span>
-          </Link>
-          <Link href="/lists" className={router.pathname === "/lists" ? 'active' : ''}>
-            {router.pathname === "/lists" ? (
-              <HiDocumentText size={28} />
-            ) : (
-              <HiOutlineDocumentText size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.lists" />
-            </span>
-          </Link>
-          <Link href="/profile" className={router.pathname.includes("/profile") ? 'active' : ''}>
-            {router.pathname.includes("/profile") ? (
-              <HiUser size={28} />
-            ) : (
-              <HiOutlineUser size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.profile" />
-            </span>
-          </Link>
-          <Link href="/settings/accessibility_display_and_languages" className={router.pathname.includes("/settings") ? 'active' : ''}>
-            {router.pathname.includes("/settings") ? (
-              <HiDotsCircleHorizontal size={28} />
-            ) : (
-              <HiOutlineDotsCircleHorizontal size={28} />
-            )}
-            <span>
-              <FormattedMessage id="page.more" />
-            </span>
-          </Link>
+          ))}
         </nav>
         <button className='btn blue-btn theme-color'>
           <FormattedMessage id="global.send_tweet" />
